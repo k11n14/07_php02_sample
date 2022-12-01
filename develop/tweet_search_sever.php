2adb83cf46b1ac1506902f5c88990b3a49877b0d
k11n14 <k11n14@icloud.com>
2022-12-01 20:45:04 +0900

    ユーザーの任意の文字でDBからデータを取ってこれる。表示はまだ。　#1

--- develop/tweet_search_sever.php
+++ develop/tweet_search_sever.php
@@ -1,10 +1,27 @@
 <?php
+echo('<pre>');
 var_dump($_POST);
+echo('</pre>');
+
+echo('<pre>');
+var_dump($_POST["search_word"]);
+echo('</pre>');
 
 // DB接続
 $dbn ='mysql:dbname=Twitter;charset=utf8mb4;port=3306;host=localhost';
 $user = 'root';
 $pwd = '';
+$search_word = $_POST["search_word"];
+
+
+
+echo('<pre>');
+var_dump($search_word) ;
+echo('</pre>');
+
+echo('<pre>');
+echo $search_word ;
+echo('</pre>');
 
 // DBに接続出来ているかの確認。おk
 try{
@@ -14,8 +31,15 @@ try{
   echo json_encode(["db error" => "{$e->getMessage()}"]);
   exit();
 }
-$sql = 'SELECT * FROM Date_table WHERE tweet=1';
+// あ〜てすとだよ 	かいけつしたで！
+
+// 'SELECT『参照』。*『全データ』。FROM テーブル名『テーブル名を指定』。WHERE カラム名=検索したいワード『検索』。ORDER BY カラム名 ASCかDESC=（昇順）（降順）『並び替え』。DESC LIMIT 数字 『表示制限』';
+$sql = 'SELECT * FROM Date_table WHERE tweet= :word ORDER BY created_at DESC LIMIT 10';
+
+  // PDO（PHP Data Objects）=異なるデータベースでも同じ命令で操作できるようにする
 $stmt = $pdo->prepare($sql);
+
+$stmt->bindValue(':word', $search_word, PDO::PARAM_STR);
 
 try {
   $status = $stmt->execute();
