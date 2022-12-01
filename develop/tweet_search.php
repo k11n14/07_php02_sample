adffc0d5fe2be0e58c5ef3d8cb674189a3fb0b48
k11n14 <k11n14@icloud.com>
2022-12-01 17:15:25 +0900

    検索機能を作る

---
+++ develop/tweet_search.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="soshinsaki.php" method="post">
+  <!-- 任意の<input>要素＝入力欄などを用意する -->
+  <input type="text" name="search_word">
+  <!-- 送信ボタンを用意する -->   
+  <input type="submit" name="submit" value="送信">
+</form>
+</body>
+</html>
